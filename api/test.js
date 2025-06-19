b47eed3ffb4525e0b3326fc621fa92a81d00cf27
acedon18 <dardan_nici@hotmail.com>
2025-06-19 10:31:31 +0200

    Ultra-simplify test API endpoint

--- api/test.js
+++ api/test.js
@@ -1,10 +1,4 @@
-// Ultra-simple test endpoint with no dependencies at all
+// Extremely simple API endpoint
 module.exports = (req, res) => {
-  // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-  // Return a static response with no dynamic content
-  return res.status(200).send('API test endpoint is working');
+  res.status(200).send('Hello from API');
 };
